6b1056f24ca0068e864c692da6e36cead8e859b1
XamierFerran <67903138+XamierFerran@users.noreply.github.com>
2020-08-03 01:58:12 -0400

    Add files via upload

---
+++ Final Projects/Xamier/RandomPasswordGenerator-BEST20.py
@@ -0,0 +1,15 @@
+import random
+
+#Produces a random password
+#Enter how long you want the password to be
+#Returns random password
+
+def randPassword(lim):
+    caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    password = ""
+    for i in range(lim):
+        password = password + caps[random.randint(0,61)]
+
+    print(password)
+
+randPassword(64)
